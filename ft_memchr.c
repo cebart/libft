3f902f97fff040b2218542efc03aff6a65dc6a77
cbartoli <cbartoli@protonmail.com>
2017-11-24 00:12:20 +0100

    save

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: cbartoli <cbartoli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 18:15:50 by cbartoli          #+#    #+#             */
-/*   Updated: 2017/11/23 18:16:00 by cbartoli         ###   ########.fr       */
+/*   Updated: 2017/11/23 22:50:13 by cbartoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 void 	*ft_memchr(const void *s, int c, size_t n)
 {
-	const char *cs;
+	char *cs;
 
-	cs = s;
-	while(n--)
+	if(s)
 	{
-		if(*cs == (unsigned char)c)
-			return ((char *)cs);
-        cs++;
+		cs = (char *)s;
+		while(n)
+		{
+			if(*cs == (unsigned char)c)
+				return (cs);
+        		cs++;
+			n--;
+		}
 	}
 	return (NULL);
 }
