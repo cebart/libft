fe4403adf165de2f78f3afb40c9801c91e718396
cbartoli <cbartoli@protonmail.com>
2017-11-24 13:20:02 +0100

    ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,23 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbartoli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cbartoli <cbartoli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/08/06 13:30:14 by cbartoli          #+#    #+#             */
-/*   Updated: 2017/08/09 15:29:56 by cbartoli         ###   ########.fr       */
+/*   Created: 2017/11/24 13:09:34 by cbartoli          #+#    #+#             */
+/*   Updated: 2017/11/24 13:16:55 by cbartoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "libft.h"
 
-static int		ft_is_negativ(const char *str)
+static int	ft_is_negativ(char c)
 {
 	int sign;
 
 	sign = 1;
-	if (*str == '-')
+	if (c == '-')
 		sign = -1;
 	return (sign);
+}
+
+static int	ft_isspace(char c)
+{
+	if (c == ' ' || c == '\n' || c == '\t' || c == '\r'
+		       	|| c == '\v' || c == '\f')
+		return (1);
+	return (0);
 }
 
 int		ft_atoi(const char *str)
@@ -28,9 +36,9 @@ int		ft_atoi(const char *str)
 	int result;
 
 	result = 0;
-	while (*str == ' ' || *str == '\n' || *str == '\t')
+	while (ft_isspace(*str))
 		str++;
-	sign = ft_is_negativ(str);
+	sign = ft_is_negativ(*str);
 	if (*str == '-' || *str == '+')
 		str++;
 	while (*str >= '0' && *str <= '9')
